35ab1c64d431c18272492ace67eff80e3de71592
Marcos Augusto Modesto <marcosaugustomodesto15@gmail.com>
2019-05-15 13:56:15 -0300

    Update Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 
 import classes from './Person.css';
-import Aux from '../../../hoc/Aux';
+import Auxilary from '../../../hoc/Auxilary';
 
 class Person extends Component {  
     render(){
         console.log('[Person.js] rendering...');  
         return (
-            <Aux>
+            <Auxilary>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </Aux>
+            </Auxilary>
         );
     }
 }
