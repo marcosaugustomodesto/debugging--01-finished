92474b6635e763c9d9bc6e367ede77d824a0ef70
marcosaugustomodesto <marcosaugustomodesto15@gmail.com>
2019-05-16 12:44:02 -0300

    setting state correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
 
@@ -63,7 +64,10 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState( { persons: persons } );
+    this.setState( { 
+      persons: persons, 
+      changeCounter: this.state.changeCounter + 1
+    } );
   }
 
   deletePersonHandler = ( personIndex ) => {
