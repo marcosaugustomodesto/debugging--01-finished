ab60de03dea1ac9f8e8615c18cc9ea1bb68bab0c
marcosaugustomodesto <marcosaugustomodesto15@gmail.com>
2019-05-16 11:32:39 -0300

    passing unknown props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
+import Auxilary from '../../../hoc/Auxilary';
+import withClass from '../../../hoc/withClass';
+import classes from './Person.css';
 
 class Person extends Component {  
     render(){
         console.log('[Person.js] rendering...');  
         return (
-            <React.Fragment>
+            <Auxilary>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </React.Fragment>
+            </Auxilary>
         );
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
