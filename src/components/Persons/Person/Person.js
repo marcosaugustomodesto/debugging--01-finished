95f0cab78ed97bd20f8e571250d4dd65be6be538
marcosaugustomodesto <marcosaugustomodesto15@gmail.com>
2019-05-16 10:28:22 -0300

    react fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,14 @@
 import React, {Component} from 'react';
 
-import classes from './Person.css';
-import Auxilary from '../../../hoc/Auxilary';
-
 class Person extends Component {  
     render(){
         console.log('[Person.js] rendering...');  
         return (
-            <Auxilary>
+            <React.Fragment>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </Auxilary>
+            </React.Fragment>
         );
     }
 }
