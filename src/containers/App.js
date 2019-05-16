69c745943d329c9a7743615a051576f62228eb93
marcosaugustomodesto <marcosaugustomodesto15@gmail.com>
2019-05-16 10:55:43 -0300

    high order component with class

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-
+import WithClass from '../hoc/WithClass';
 class App extends Component {
 
   constructor(props){
@@ -87,7 +87,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={()=>{this.setState({showCockpit:false})}}>Remove Cockpit</button>
         {this.state.showCockpit? (
         <Cockpit 
@@ -98,7 +98,7 @@ class App extends Component {
         />
         ): null}
         {persons}
-      </div>
+      </WithClass>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
